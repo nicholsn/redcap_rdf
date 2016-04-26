57b4601bc14a4543eddfae8242f7ae63a2be813c
Victor <victor.meyerson@sri.com>
2016-04-26 15:43:03 -0700

    Converted the majority of the script into a class and added summary data.

--- redcap_rdf/datadict_validator.py
+++ redcap_rdf/datadict_validator.py
@@ -24,125 +24,148 @@ TEXT_MAX = "Text Validation Max"
 HEADERS = [FIELD_NAME, FORM, FIELD_TYPE, FIELD_LABEL, CHOICES, TEXT_TYPE,
            TEXT_MIN, TEXT_MAX]
 
-# TODO: make these fields more flexible for other validations
-FIXED_ROWS = ["subject", "arm", "visit"]
 
+class Validator:
+    def __init__(self):
+        # clear internal data structures
+        self._warnings = {}
+        self._errors = {}
 
-def check_headers(headers):
-    """Verify Header Columns
-
-    Parameters
-    ----------
-    headers : iterable object
-        Instance of `csv.DictReader` for a REDCap data dictionary in csv format.
-
-    Returns
-    -------
-    bool
-        True indicates an error in the data dictionary header.
-    """
-    ret_val = False
-    for field in HEADERS:
-        if field not in headers:
-            print("Could not find: {} in the header".format(field))
-            ret_val = True
-    return ret_val
-
-
-def check_row(row, number):
-    message = "form: {}, field: {}, value type: {}, line: {}"
-    print(message.format(row[FORM], row[FIELD_NAME], row[FIELD_TYPE], number))
-    check_value_type(row)
-
-
-def check_value_type(row):
-    if row[FIELD_TYPE] == "dropdown":
-        return validate_dropdown(row[CHOICES])
-    elif row[FIELD_TYPE] == "yesno":
-        return validate_yes_no(row)
-    elif row[FIELD_TYPE] == "text":
-        return validate_text(row)
-    else:
-        val_type = row[FIELD_TYPE]
-        print("WARNING: Skipping validation of type: '{}'".format(val_type))
-
-
-def validate_dropdown(choices_str):
-    ret_val = False
-    choices = choices_str.split('|')
-    if len(choices) <= 1:
-        print "There should be more than one choice"
-    for choice in choices:
-        breakdown = choice.split(",")
-        if len(breakdown) != 2:
-            print("This is an invalid choice: {}".format(choice))
-            ret_val = True
-    return ret_val
-
-
-def validate_yes_no(row):
-    ret_val = False
-    if len(row[CHOICES]) > 0:
-        print("YesNo field should not have choices")
-        ret_val = True
-    return ret_val
-
-
-def validate_text(row):
-    ret_val = False
-    print("  Item: {} is a {}".format(row[FIELD_NAME], row[TEXT_TYPE]))
-    if row[TEXT_TYPE] == "number":
-        ret_val = validate_numeric_range(row[TEXT_MIN], row[TEXT_MAX])
-    else:
-        ret_val = True
-        print("  No validation rules for type: '{}'".format(row[TEXT_TYPE]))
-    return ret_val
-
-
-def validate_numeric_range(low_str, high_str):
-    ret_val = False
-    print("  Range: [{},{}]".format(low_str, high_str))
-    low = float(low_str)
-    if high_str != "":
-        high = float(high_str)
-        if high < low:
-            ret_val = True
-            message = "  Max value ({}) should not be less than min value ({})"
-            print(message.format(high_str, low_str))
-    else:
-        print("WARNING: no maximum value set")
-    return ret_val
-
-
-def process(dd, fixed_rows):
-    if not os.path.isfile(dd):
-        print("{} file not found".format(dd))
-        return
-    print("Processing: {}".format(dd))
-
-    if fixed_rows:
-        print("Running extra check for first rows")
-
-    with open(dd) as f:
-        reader = csv.DictReader(f)
-        # check existence of headers
-        if check_headers(reader.fieldnames):
-            print("ERROR: Header check FAILED!")
+    def process(self, dd, first_rows):
+        if not os.path.isfile(dd):
+            print("{} file not found".format(dd))
             return
-        # check each row
-        tmp_val = False
-        tmp_counter = 0
-        for row in reader:
-            tmp_val = check_row(row, reader.line_num) or tmp_val
-            if fixed_rows and tmp_counter < len(FIXED_ROWS):
-                if row[FIELD_NAME] != FIXED_ROWS[tmp_counter]:
-                    message = "ERROR: field should be '{}' found '{}'"
-                    print(message.format(FIXED_ROWS[tmp_counter],
-                                         row[FIELD_NAME]))
-                    tmp_val = True
-                tmp_counter += 1
-        if tmp_val:
-            print("ERROR: There was a failure in at least one row")
+        print("Processing: {}".format(dd))
+
+        if len(first_rows) > 0:
+            print("Running extra check for first rows")
+
+        with open(dd) as f:
+            reader = csv.DictReader(f)
+
+            # check headers
+            self._check_headers(reader.fieldnames)
+
+            # check each row
+            tmp_counter = 0
+            for row in reader:
+                self._check_row(row, reader.line_num)
+                if tmp_counter < len(first_rows):
+                    if row[FIELD_NAME] != first_rows[tmp_counter]:
+                        message = "field should be '{}' found '{}'"
+                        msg = message.format(first_rows[tmp_counter],
+                                             row[FIELD_NAME])
+                        self._append_error(row[FIELD_NAME], msg)
+                    tmp_counter += 1
+        self._print_summary()
+
+    # check functions
+    def _check_headers(self, headers):
+        """Verify Header Columns
+
+        Parameters
+        ----------
+        headers : iterable object
+            Instance of `csv.DictReader` for a REDCap data dictionary in csv format.
+        """
+        for field in HEADERS:
+            if field not in headers:
+                msg = "Could not find: '{}' in the header".format(field)
+                self._append_error("HEADERS", msg)
+
+    def _check_row(self, row, number):
+        message = "form: {}, field: {}, value type: {}, line: {}"
+        print(message.format(row[FORM], row[FIELD_NAME],
+                             row[FIELD_TYPE], number))
+        self._check_value_type(row)
+
+    def _check_value_type(self, row):
+        if row[FIELD_TYPE] == "dropdown":
+            self._validate_dropdown(row[FIELD_NAME], row[CHOICES])
+        elif row[FIELD_TYPE] == "yesno":
+            self._validate_yes_no(row[FIELD_NAME], row)
+        elif row[FIELD_TYPE] == "text":
+            self._validate_text(row[FIELD_NAME], row)
+        else:
+            val_type = row[FIELD_TYPE]
+            msg = "Skipping validation of type: '{}'".format(val_type)
+            self._append_warning(row[FIELD_NAME], msg)
+
+    # validate various field type functions
+    def _validate_dropdown(self, field, choices_str):
+        choices = choices_str.split('|')
+        if len(choices) <= 1:
+            msg = "There should be more than one choice"
+            self._append_error(field, msg)
+        for choice in choices:
+            breakdown = choice.split(",")
+            if len(breakdown) != 2:
+                msg = "This is an invalid choice: {}".format(choice)
+                self._append_error(field, msg)
+
+    def _validate_yes_no(self, field, row):
+        if len(row[CHOICES]) > 0:
+            msg = "YesNo field should not have choices"
+            self._append_error(field, msg)
+
+    def _validate_text(self, field, row):
+        print("  Item: {} is a {}".format(row[FIELD_NAME], row[TEXT_TYPE]))
+        if row[TEXT_TYPE] == "number":
+            self._validate_numeric_range(field, row[TEXT_MIN], row[TEXT_MAX])
+        else:
+            msg = "No validation rules for type: '{}'".format(row[TEXT_TYPE])
+            self._append_warning(field, msg)
+
+    def _validate_numeric_range(self, field, low_str, high_str):
+        print("  Range: [{},{}]".format(low_str, high_str))
+        low = float(low_str)
+        if high_str != "":
+            high = float(high_str)
+            if high < low:
+                msg = "Max value ({}) should not be less than min value ({})"
+                self._append_error(field, msg.format(high_str, low_str))
+        else:
+            msg = "no maximum value set"
+            self._append_warning(field, msg)
+
+    # accumulate messages
+    def _append_error(self, key, msg):
+        if key not in self._errors:
+            self._errors[key] = [msg]
+        else:
+            self._errors[key].append(msg)
+
+    def _append_warning(self, key, msg):
+        if key not in self._warnings:
+            self._warnings[key] = [msg]
+        else:
+            self._warnings[key].append(msg)
+
+    # print helpers
+    def _print_summary(self):
+        print("SUMMARY")
+        print("-------")
+        message = "There are {} error(s)."
+        print(message.format(len(self._errors)))
+        self._print_details(self._errors)
+        print("")
+        message = "There are {} warning(s)."
+        print(message.format(len(self._warnings)))
+        self._print_details(self._warnings)
+
+    def _print_details(self, ds):
+        for (field, msgs) in ds.items():
+            print("Field: '{}'".format(field))
+            for msg in msgs:
+                print("  {}".format(msg))
+
+    # 'private' member data
+    _warnings = {}
+    _errors = {}
+
+
+def csv_to_list(arg):
+    return map(str, arg.split(','))
 
 
 def main():
@@ -150,14 +173,16 @@ def main():
     parser.add_argument("-d", "--datadict",
                         help="the data dictionary csv file",
                         type=str)
-    parser.add_argument("--fixed-rows",
+    parser.add_argument("--first-rows",
                         help="applies an extra check to the first rows",
-                        action="store_true")
+                        type=csv_to_list,
+                        default=[])
     parser.add_argument("-v", "--verbose",
                         help="increase output verbosity",
                         action="store_true")
     args = parser.parse_args()
-    process(args.datadict, args.fixed_rows)
+    validator = Validator()
+    validator.process(args.datadict, args.first_rows)
 
 
 if __name__ == "__main__":
