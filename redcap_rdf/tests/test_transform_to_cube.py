281a715814ebd181c70eadc4294122a58a815a48
Nolan Nichols <nolan.nichols@gmail.com>
2016-06-10 14:27:04 -0700

    tst: know subject is in built graph

--- redcap_rdf/tests/test_transform_to_cube.py
+++ redcap_rdf/tests/test_transform_to_cube.py
@@ -4,9 +4,21 @@
 ##
 import pytest
 
+from redcap_rdf.tests.data import test_files
 from redcap_rdf.transform_to_cube import Transformer
 
 
 def test_init():
     transformer = Transformer()
-    assert isinstance(transformer,Transformer)
+    assert isinstance(transformer, Transformer)
+
+
+def test_build_graph():
+    dd = test_files.get('observation_datadict')
+    config = test_files.get('mapping')
+    transformer = Transformer()
+    transformer.build_graph(dd, config)
+    subjects = [i.n3() for i in transformer._g.subjects()]
+    assert '<http://ncanda.sri.com/terms.ttl#subject>' in subjects
+
+
